00c618109549758733bba56e881365abcd6d9250
Atkrye <tja511@york.ac.uk>
2016-05-05 16:18:36 +0100

    Version 1.2 Narrative is now correctly broken into new lines. ZpletIOAgent.py is now passing agent and game arguments to the jar to allow different games to be played. Resources folder provides a couple of resources to use with your agent.

--- Python - Example Project/ZpletIOAgent.py
+++ Python - Example Project/ZpletIOAgent.py
@@ -1,24 +1,26 @@
 from subprocess import Popen, PIPE, STDOUT
 from random import randint
 import binascii
-def startZplet(jardir):
-    p = Popen(['java', '-jar', jardir], stdin=PIPE, stdout=PIPE)
-    print("Begin")
+def startZplet(jardir, gamedir):
+    p = Popen(['java', '-jar', jardir, 'ieeecig.advent.IOAgent', gamedir], stdin=PIPE, stdout=PIPE)
     return (readNarrative(p),p)
 
 def postCommand(p, command):
-    print(command)
+    print("Response: " + command)
     p.stdin.write(bytes(command + "\n", "ascii"))
     p.stdin.flush()
     return readNarrative(p)
 
 def readNarrative(p):
     narrative=""
-    for line in p.stdout:
-        if(line.decode("utf-8").startswith("BREAK-NARRATIVE")):
-            print("BREAK")
-            break
-        narrative = narrative + line.decode("utf-8") + "\n"
+    cont = True
+    while cont:
+        for line in p.stdout:
+            if(line.decode("utf-8").startswith("BREAK-NARRATIVE")):
+                print("BREAK")
+                cont = False
+                break
+            narrative = narrative + line.decode("utf-8") + "\n"
     return narrative
 
 def action(narrative):
@@ -30,7 +32,7 @@ def action(narrative):
     return command
 
 print("Booting Z Machine...")
-ret = startZplet('./IOieee-cig-advent-1.0.jar')
+ret = startZplet('../Example Project/lib3rd/ieee-cig-advent-1.2.jar','../resources/Advent.z5')
 narrative = ret[0]
 p = ret[1]
 print("Z Machine Launched")
